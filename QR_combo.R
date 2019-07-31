6a50d8aaaf32c1eb79c3d6027025a05e718bfa47
Megan <megan.chiovaro@uconn.edu>
2019-07-31 11:40:57 -0400

    filled fourth corner

--- QR_combo.R
+++ QR_combo.R
@@ -13,16 +13,20 @@ stimuli <- data.frame(image_list)
 names(stimuli) <- c("image")
 
 #  filling top right corner with marker 1
-stimuli$rightup <- rep("marker_01",nrow(stimuli))
+stimuli$rightup <- rep("marker_01.png",nrow(stimuli))
 
 # filling top left corner half with marker 2 and half with 3
-stimuli$leftup <- rep("marker_02",nrow(stimuli))
-stimuli$leftup[30:58] <- "marker_03"
+stimuli$leftup <- rep("marker_02.png",nrow(stimuli))
+stimuli$leftup[30:58] <- "marker_03.png"
 
 # filling lower right corner - further  distinguishing with markers 4 & 5
-stimuli$rightdown <- rep("marker_04", nrow(stimuli))
-stimuli$rightdown[15:29] <- "marker_05"
-stimuli$rightdown[44:58] <- "marker_05"
+stimuli$rightdown <- rep("marker_04.png", nrow(stimuli))
+stimuli$rightdown[15:29] <- "marker_05.png"
+stimuli$rightdown[44:58] <- "marker_05.png"
 
-stimuli$leftdown <- rep("marker_06", nrow(stimuli))
-stimuli$leftdown[] <- 
+# filling lower left corner by cycling through unused markers
+stimuli$leftdown <- rep("marker_06.png", nrow(stimuli))
+stimuli$leftdown[1:14] <- qr_list[7:20]
+stimuli$leftdown[15:29] <- qr_list[7:21]
+stimuli$leftdown[30:43] <- qr_list[7:20]
+stimuli$leftdown[44:58] <- qr_list[7:21]
