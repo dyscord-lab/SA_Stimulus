ab3e9cda3fe6cfe9b2bc1ed66e92e6a64543de69
Megan <megan.chiovaro@uconn.edu>
2019-07-31 11:35:23 -0400

    filled first three corners with markers

--- QR_combo.R
+++ QR_combo.R
@@ -8,5 +8,21 @@ qr_list <- list.files()
 
 qr_counter <- 1
 
+# adding image names to  csv
 stimuli <- data.frame(image_list)
 names(stimuli) <- c("image")
+
+#  filling top right corner with marker 1
+stimuli$rightup <- rep("marker_01",nrow(stimuli))
+
+# filling top left corner half with marker 2 and half with 3
+stimuli$leftup <- rep("marker_02",nrow(stimuli))
+stimuli$leftup[30:58] <- "marker_03"
+
+# filling lower right corner - further  distinguishing with markers 4 & 5
+stimuli$rightdown <- rep("marker_04", nrow(stimuli))
+stimuli$rightdown[15:29] <- "marker_05"
+stimuli$rightdown[44:58] <- "marker_05"
+
+stimuli$leftdown <- rep("marker_06", nrow(stimuli))
+stimuli$leftdown[] <- 
