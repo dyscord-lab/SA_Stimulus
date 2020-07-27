ab0900583cbb2058acb1a682e54f950c0e33d687
Megan Chiovaro <megan.chiovaro@uconn.edu>
2020-07-27 11:06:58 -0400

    Update new IAPS filtering

--- IAPS.R
+++ IAPS.R
@@ -1,11 +1,17 @@
 #### IAPS: Choosing photos for SA Phase II ####
-# To use, create subdirectory './IAPS/' and add tech report file 'AllSubjects_1-20.txt'
-# This subdir is in the .gitignore per instructions to keep it hidden from IAPS directors
+#
+# To use, create subdirectory './IAPS/' and add tech report
+# file 'AllSubjects_1-20.txt'. This subdir is in the 
+# .gitignore per instructions to keep it hidden from IAPS 
+# directors
+#
+# By: @mchiovaro
+# Last updated: 07_27_2020
 
 # set working directory
 setwd("./SA_Stimulus/")
 
-# Install and load packages
+# install and load packages
 #install.packages("reader")
 library(reader)
 library(dplyr)
@@ -25,38 +31,69 @@ metrics = metrics[,c(1, 2, 3, 5)]
 # look at means for valence and arousal
 mean(metrics$valmn)
 mean(metrics$aromn)
-# means are both around 5
 
 # looking at % for cutting top and bottom rated pics
-(16/nrow(metrics))/2 # .67% would be top 8 pics
-(14/nrow(metrics))/2 # .59% would be top 7 pics
+(15/nrow(metrics)) # 1.26% would be top 15 pics 
+(30/nrow(metrics)) # 2.51% would be top 30 pics 
 
-# 5% of photos
-.05*nrow(metrics) # take top 60
+# identify 10% of photos so there is room for pairing
+num_pics <- ceiling(.10*nrow(metrics))
 
-# checking out descriptions of photos
+# identify top 5% so most explicit photos can be filtered out
+num_skip <- ceiling(.05*nrow(metrics))
+
+# arrange by ascending valence
 arrange_val <- metrics %>%
   arrange(valmn) 
-unique(arrange_val$desc) # 611 levels
+
+# checking out descriptions of photos
+unique(sort(arrange_val$desc))
 
 # filtering out some explicit topics
-filtered <- arrange_val[!(arrange_val$desc == "BurnVictim" | arrange_val$desc == "Mutilation" | arrange_val$desc == "DeadBody" | arrange_val$desc == "HeadlessBody" | arrange_val$desc == "HurtDog" | arrange_val$desc == "Hanging" | arrange_val$desc == "InjuredChild" | arrange_val$desc == "InjuredDog" | arrange_val$desc == "SlicedHand"  | arrange_val$desc == "DeadMan" | arrange_val$desc == "Vomit" | arrange_val$desc == "ManOnFire" | arrange_val$desc == "BurntFace" | arrange_val$desc == "BatteredFem" | arrange_val$desc == "DeadDog" | arrange_val$desc == "DyingMan" | arrange_val$desc == "Execution" | arrange_val$desc == "BloodyKiss" | arrange_val$desc == "Suicide" | arrange_val$desc == "DeadTiger" | arrange_val$desc == "SeveredHand" | arrange_val$desc == "DeadCows" | arrange_val$desc == "BeatenFem"), ]
-# check that it worked
-unique(filtered$desc)
+filtered <- arrange_val[!(arrange_val$desc == "BurnVictim" | 
+                            arrange_val$desc == "Mutilation" | 
+                            arrange_val$desc == "DeadBody" | 
+                            arrange_val$desc == "HeadlessBody" | 
+                            arrange_val$desc == "HurtDog" | 
+                            arrange_val$desc == "Hanging" | 
+                            arrange_val$desc == "InjuredChild" | 
+                            arrange_val$desc == "InjuredDog" | 
+                            arrange_val$desc == "SlicedHand"  | 
+                            arrange_val$desc == "DeadMan" | 
+                            arrange_val$desc == "Vomit" | 
+                            arrange_val$desc == "ManOnFire" | 
+                            arrange_val$desc == "BurntFace" | 
+                            arrange_val$desc == "BatteredFem" | 
+                            arrange_val$desc == "DeadDog" | 
+                            arrange_val$desc == "DyingMan" | 
+                            arrange_val$desc == "Execution" | 
+                            arrange_val$desc == "BloodyKiss" | 
+                            arrange_val$desc == "Suicide" | 
+                            arrange_val$desc == "DeadTiger" | 
+                            arrange_val$desc == "SeveredHand" | 
+                            arrange_val$desc == "DeadCows" | 
+                            arrange_val$desc == "BeatenFem"), ]
 
-# cutting top 5% of valence photos to then visually approve of top pairs (4 pairs; .67%)
+# check new descriptions
+unique(sort(filtered$desc))
+
+### low valence ###
+
+# skip top 5% of valence photos to remove explixit once, 
+# then cut next 10% to keep for pairing
 low_val =  filtered %>%
   arrange(valmn) %>%
-  slice(61:120)
+  slice(num_skip:(num_skip+num_pics-1))
 
-  # take avg valence
-  mean(low_val$valmn)
+# take avg valence
+mean(low_val$valmn)
 
-# cutting bottom 5-10% of valence photos to then visually approve of top pairs (4 pairs; .67%)
+### high valence ###
+  
+# cut highest 10% of valence photos to keep for pairing
 high_val =  filtered %>%
   arrange(desc(valmn)) %>%
-  slice(1:60)
+  slice(1:num_pics)
 
-  # take avg valence
-  mean(high_val$valmn)
-  
+# take avg valence
+mean(high_val$valmn)
